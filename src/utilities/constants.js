68ef7b20e1d32d0d35837503b52c26f5d123644a
Apoorv Jain <apjtestemail@gmail.com>
2023-03-25 18:08:50 +0530

    ok

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,6 @@
 // const SERVER_ORIGIN = "http://localhost:5000";
 // const SERVER_ORIGIN = "https://yuva-backend-v2.onrender.com";
+
 const SERVER_ORIGIN = "https://yuvaportal.onrender.com";
 
 const vars = {
