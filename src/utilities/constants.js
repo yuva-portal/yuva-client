68f0a84b2fa21c30dff2053b91bb4ed6e36b13b0
Anurag <anuragtiw112@gmail.com>
2023-08-31 16:23:05 +0530

    VPS deploy changes

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,4 +1,5 @@
-const SERVER_ORIGIN = "http://localhost:8800";
+// const SERVER_ORIGIN = "http://localhost:8800";
+const SERVER_ORIGIN = "http://3.6.121.136:8080";
 // const SERVER_ORIGIN = "https://yuvaportal.onrender.com";
 // const SERVER_ORIGIN = "https://yuva-portal-server-1.onrender.com";
 // const SERVER_ORIGIN = process.env.REACT_APP_API_URL;
