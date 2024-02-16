f2af5bfc78ec26892315d802657971754f2a69c8
Aditya <adityashrivastava2903@gmail.com>
2024-02-16 11:16:52 +0530

    Add forgot password button and improve user login page design

--- src/components/common/LoginForm.jsx
+++ src/components/common/LoginForm.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Spinloader from "./Spinloader";
 
 // My css
 import css from "../../css/common/login-form.module.css";
@@ -8,54 +9,139 @@ import css from "../../css/common/login-form.module.css";
 import { validation } from "../../utilities/constants";
 
 export const LoginForm = (props) => {
-  const handleChange = (e) => {
-    props.onChange(e); // Need to pass the whole event, passing updatedField just gives the last entered character of the input
-  };
+    const [modal, setModalOpen] = useState(false);
+    const [loader, setLoader] = useState(false);
 
-  const handleLogInClick = () => {
-    props.onClick();
-  };
+    const handleChange = (e) => {
+        props.onChange(e); // Need to pass the whole event, passing updatedField just gives the last entered character of the input
+    };
 
-  return (
-    <div className={css.outerDiv}>
-      <p className={css.heading}>Login</p>
+    const handleLogInClick = () => {
+        props.onClick();
+    };
 
-      <input
-        className={css.input}
-        type="text"
-        placeholder={props.role === "user" ? "Username or Email" : "Admin Id"}
-        name={props.role === "user" ? "userId" : "adminId"}
-        value={props.role === "user" ? props.userId : props.adminId}
-        onChange={handleChange}
-        maxLength={validation.authForm.userId.maxLen}
-        autoComplete="off"
-      />
+    function handleForgotPass() {
+        setTimeout(() => {
+            setModalOpen(true);
+        }, 1000);
+        setLoader(true);
+        setTimeout(() => {
+            setLoader(false);
+        }, 1000);
+    }
 
-      <input
-        className={css.input}
-        type="password"
-        placeholder="Password"
-        name="password"
-        value={props.password}
-        onChange={handleChange}
-        maxLength={validation.authForm.password.maxLen}
-        autoComplete="off"
-      />
-      <button
-        className={css.btn}
-        onClick={handleLogInClick}
-        disabled={props.isBtnDisabled}
-      >
-        {props.isBtnDisabled ? "Logging in ..." : "Login"}
-      </button>
-      {props.role === "user" ? (
-        <>
-          <p className={css.forgotPassText}>Don't have an account ?</p>
-          <Link className={css.registerText} to="/user/register">
-            Register
-          </Link>
-        </>
-      ) : null}
-    </div>
-  );
+    function handleModalClose() {
+        setModalOpen(false);
+    }
+
+    // const modalStyles = {
+    //     content: {
+    //         position: "relative",
+    //         width: "30rem",
+    //         height: "30rem",
+    //         backgroundColor: "red",
+    //         left: "30vw",
+    //         top: "10%",
+    //     },
+    // };
+
+    return (
+        <div className="loginWrapper">
+            <div className={css.outerDiv}>
+                {/* {modal && <Modal isOpen={modal} style={modalStyles} />} */}
+                {modal ? (
+                    <div className={css.loginModal}>
+                        <p className={css.passHeading}>Recover Password</p>
+                        {/* <label htmlFor="newPassEmail"> */}
+                        <input
+                            type="email"
+                            name=""
+                            id="newPassEmail"
+                            placeholder="Recovery E-mail"
+                            className={css.forgotPassInput}
+                        />
+                        {/* </label> */}
+                        <div className={css.forgotPassBtnWrapper}>
+                            <button
+                                className={css.forgotPassCancelBtn}
+                                onClick={handleModalClose}
+                            >
+                                Cancel
+                            </button>
+                            <button className={css.forgotPassBtn}>
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <p className={css.heading}>Login</p>
+                        <input
+                            className={css.input}
+                            type="text"
+                            placeholder={
+                                props.role === "user"
+                                    ? "Username or Email"
+                                    : "Admin Id"
+                            }
+                            name={props.role === "user" ? "userId" : "adminId"}
+                            value={
+                                props.role === "user"
+                                    ? props.userId
+                                    : props.adminId
+                            }
+                            onChange={handleChange}
+                            maxLength={validation.authForm.userId.maxLen}
+                            autoComplete="off"
+                        />
+
+                        <input
+                            className={css.input}
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            value={props.password}
+                            onChange={handleChange}
+                            maxLength={validation.authForm.password.maxLen}
+                            autoComplete="off"
+                        />
+
+                        <Link
+                            onClick={handleForgotPass}
+                            className={css.forgotPassText}
+                        >
+                            Forgot Password
+                        </Link>
+                        {loader && (
+                            <div className={css.forgotPassLoaderWrapper}>
+                                <div className={css.forgotPassLoader}>
+                                    <Spinloader />
+                                </div>
+                            </div>
+                        )}
+                        <button
+                            className={css.btn}
+                            onClick={handleLogInClick}
+                            disabled={props.isBtnDisabled}
+                        >
+                            {props.isBtnDisabled ? "Logging in ..." : "Login"}
+                        </button>
+                        {props.role === "user" ? (
+                            <>
+                                <p className={css.forgotPassText}>
+                                    Don't have an account ?
+                                </p>
+                                <Link
+                                    className={css.registerText}
+                                    to="/user/register"
+                                >
+                                    Register
+                                </Link>
+                            </>
+                        ) : null}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
